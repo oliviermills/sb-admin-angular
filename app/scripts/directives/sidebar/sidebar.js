3dec59f5d2e9dea6cd9172973361c55dfb6351c4
Olivier Mills <olivier@millsitconsulting.com>
2015-11-29 17:58:12 -0700

    sidebar partial fix of scope

--- app/scripts/directives/sidebar/sidebar.js
+++ app/scripts/directives/sidebar/sidebar.js
@@ -13,28 +13,30 @@ angular.module('sbAdminApp')
       templateUrl:'scripts/directives/sidebar/sidebar.html',
       restrict: 'E',
       replace: true,
-      scope: {
-      },
-      controller:function($scope){
-        $scope.selectedMenu = 'dashboard';
-        $scope.collapseVar = 0;
-        $scope.multiCollapseVar = 0;
-        
-        $scope.check = function(x){
-          
-          if(x==$scope.collapseVar)
-            $scope.collapseVar = 0;
-          else
-            $scope.collapseVar = x;
-        };
-        
-        $scope.multiCheck = function(y){
-          
-          if(y==$scope.multiCollapseVar)
-            $scope.multiCollapseVar = 0;
-          else
-            $scope.multiCollapseVar = y;
-        };
-      }
+      //scope: true
+      //controller:[]
     }
+  }])
+  .controller('sidebarCtrl', ['$scope', function($scope) {
+
+      $scope.selectedMenu = 'dashboard';
+      $scope.collapseVar = 0;
+      $scope.multiCollapseVar = 0;
+
+      $scope.check = function(x){
+
+        if(x==$scope.collapseVar)
+          $scope.collapseVar = 0;
+        else
+          $scope.collapseVar = x;
+      };
+
+      $scope.multiCheck = function(y){
+
+        if(y==$scope.multiCollapseVar)
+          $scope.multiCollapseVar = 0;
+        else
+          $scope.multiCollapseVar = y;
+      };
+
   }]);
